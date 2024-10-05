1551a83a086211587fd9e04f391ccbb7cc46f76b
angelporlan <angeelporlan4@gmail.com>
2024-10-05 22:43:41 +0200

    jwt returns the username and changed so that the username of user is unique

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -11,6 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_USERNAME', fields: ['username'])]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
