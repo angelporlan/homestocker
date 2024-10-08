d97db17fad5ead4ba82348c1c82e69eff675fbd9
angelporlan <angeelporlan4@gmail.com>
2024-10-08 18:00:25 +0200

    component navbar created

---
+++ frontend/src/app/views/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './dashboard.component.html',
+  styles: ``
+})
+export class DashboardComponent {
+
+}
