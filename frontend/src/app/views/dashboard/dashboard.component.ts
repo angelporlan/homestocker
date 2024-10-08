ecdffe4e8cb6c4f33c8c12be219c64cb854e4ed5
angelporlan <angeelporlan4@gmail.com>
2024-10-08 23:17:29 +0200

    first version of sidebar component

--- frontend/src/app/views/dashboard/dashboard.component.ts
+++ frontend/src/app/views/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, SidebarComponent],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
