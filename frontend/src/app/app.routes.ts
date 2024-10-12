48858c1b72345847e8bd362771a7868bbb2bdeb1
angelporlan <angeelporlan4@gmail.com>
2024-10-12 15:16:59 +0200

    feat: initialize child components of DashboardComponent and define their routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,13 @@ import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { LandingPageComponent } from './views/landing-page/landing-page.component';
 import { AuthComponent } from './views/auth/auth.component';
 import { HomesComponent } from './views/houses/houses.component';
+
+// dashboard components
 import { HomeComponent } from './components/dashboard/home/home.component';
+import { ProductsComponent } from './components/dashboard/products/products.component';
+import { UsersComponent } from './components/dashboard/users/users.component';
+import { NotificationsComponent } from './components/dashboard/notifications/notifications.component';
+import { SettingsComponent } from './components/dashboard/settings/settings.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent},
@@ -11,6 +17,10 @@ export const routes: Routes = [
     { path: 'houses', component: HomesComponent},
     { path: 'dashboard/:id', component: DashboardComponent, children: [
         { path: 'home', component: HomeComponent },
+        { path: 'products', component: ProductsComponent},
+        { path: 'users', component: UsersComponent},
+        { path: 'notifications', component: NotificationsComponent},
+        { path: 'settings', component: SettingsComponent}, 
     ]},
 
 ];
