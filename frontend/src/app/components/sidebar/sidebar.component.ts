a7b4cd2bdb62341061b50ecbaf897f9056f443f3
angelporlan <angeelporlan4@gmail.com>
2024-10-12 15:53:36 +0200

    Highlight active route in sidebar based on current URL

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,41 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  currtentRoute: string = '';
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  navigateTo(path: string): void {
+    let currentRoute = this.router.url;
+    let segments = currentRoute.split('/');
+    segments.pop();
+    segments.push(path);
+    const newRoute = segments.join('/');
+    this.router.navigate([newRoute]);
+  }
+  
+  isActive(path: string): boolean {
+    // Obtener la URL actual
+    const currentRoute = this.router.url;
+  
+    // Dividir la URL en segmentos
+    const segments = currentRoute.split('/');
+  
+    // Recuperar el último segmento
+    const lastSegment = segments.pop() || ''; // Si el array está vacío, usa una cadena vacía
+  
+    // Comparar el último segmento con el argumento 'path'
+    return lastSegment === path;
+  }
+  
 
 }
