ac297ea3e9dbed03a3118e2cb5b7daa906cf447d
angelporlan <angeelporlan4@gmail.com>
2024-10-13 10:40:55 +0200

    select responsive in products component

--- frontend/src/app/components/dashboard/products/products.component.ts
+++ frontend/src/app/components/dashboard/products/products.component.ts
@@ -1,12 +1,26 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [NgClass, FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  selectedClass: string = '';
+  selectedOption: string = 'soon';
 
+  onSelectChange() {
+    if (this.selectedOption === 'alphabetical') {
+      console.log('alphabetical');
+      this.selectedClass = 'alphabetical';
+    } else if (this.selectedOption === 'last') {
+      this.selectedClass = 'last';
+    } else {
+      this.selectedClass = 'soon';
+    }
+  }
 }
