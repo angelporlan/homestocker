b9c140934e273b3a582fd42ece0de2fd2c883522
angelporlan <angeelporlan4@gmail.com>
2024-10-14 23:08:44 +0200

    limit display of users and products to 2 each on homepage

--- frontend/src/app/components/dashboard/home/home.component.ts
+++ frontend/src/app/components/dashboard/home/home.component.ts
@@ -4,6 +4,7 @@ import { UserBoxComponent } from './user-box/user-box.component';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from '../../loader/loader.component';
 import { ProductBoxComponent } from './product-box/product-box.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -17,9 +18,14 @@ export class HomeComponent implements OnInit {
   houseUsers: any[] = [];
   houseDetails: any = {};
   numberOfProducts: number = 0;
-  isLoading: boolean = true; 
+  isLoading: boolean = true;
+  
+  twoHouseUsers: any[] = [];
+  twoHouseProducts: any[] = [];
+  isTwoHouseUsersVisible: boolean = false;
+  isTwoHouseProductsVisible: boolean = false;
 
-  constructor(private dashboardService: DashboardService) {}
+  constructor(private dashboardService: DashboardService, private router: Router) {}
 
   ngOnInit(): void {
     this.isLoading = true; 
@@ -63,12 +69,36 @@ export class HomeComponent implements OnInit {
         this.isLoading = false;
       }
     });
+  }
 
+  addTwoHouseUsers(): void {
+    this.twoHouseUsers = this.houseUsers.slice(0, 2);
+    if (this.houseUsers.length > 2) {
+      this.isTwoHouseUsersVisible = true;
+    }
+  }
+
+  addTwoHouseProducts(): void {
+    this.twoHouseProducts = this.houseProducts.slice(0, 2);
+    if (this.houseProducts.length > 2) {
+      this.isTwoHouseProductsVisible = true;
+    }
   }
 
   checkIfLoadingComplete(): void {
     if (this.houseUsers.length > 0 ) {
       this.isLoading = false;
+      this.addTwoHouseProducts();
+      this.addTwoHouseUsers();
     }
+  }
+
+  navigateTo(path: string): void {
+    const currentRoute = this.router.url;
+    const segments = currentRoute.split('/');
+    const dashboardIndex = segments.indexOf('dashboard');
+    const id = segments[dashboardIndex + 1];
+    const newRoute = `dashboard/${id}/${path}`;
+    this.router.navigate([newRoute]);
   }
 }
