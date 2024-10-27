8c5813142fc73df9312d3d468563d1277cdbf156
angelporlan <angeelporlan4@gmail.com>
2024-10-27 12:04:43 +0100

    pre-article component of landing page finished

---
+++ frontend/src/app/components/landing-page/pre-article/pre-article.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-pre-article',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './pre-article.component.html',
+  styleUrl: './pre-article.component.css'
+})
+export class PreArticleComponent {
+
+}
--- frontend/src/app/views/landing-page/landing-page.component.ts
+++ frontend/src/app/views/landing-page/landing-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { DashboardComponent } from "../dashboard/dashboard.component";
 import { ImageLettersComponent } from '../../components/landing-page/image-letters/image-letters.component';
+import { PreArticleComponent } from '../../components/landing-page/pre-article/pre-article.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavbarComponent, DashboardComponent, ImageLettersComponent],
+  imports: [NavbarComponent, DashboardComponent, ImageLettersComponent, PreArticleComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
