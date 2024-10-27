711e47599c320d0cbad0fdecba84b9da8889e418
angelporlan <angeelporlan4@gmail.com>
2024-10-27 11:47:38 +0100

    image-letter component of landing page finished

---
+++ frontend/src/app/components/landing-page/image-letters/image-letters.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-image-letters',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './image-letters.component.html',
+  styleUrl: './image-letters.component.css'
+})
+export class ImageLettersComponent {
+
+}
--- frontend/src/app/views/landing-page/landing-page.component.ts
+++ frontend/src/app/views/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { DashboardComponent } from "../dashboard/dashboard.component";
+import { ImageLettersComponent } from '../../components/landing-page/image-letters/image-letters.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavbarComponent, DashboardComponent],
+  imports: [NavbarComponent, DashboardComponent, ImageLettersComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
