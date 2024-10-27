cc90a316c7a1ce8baf3b8cc64d9a1a3a47c6d142
angelporlan <angeelporlan4@gmail.com>
2024-10-27 16:51:00 +0100

    articles component of landing page finished

--- frontend/src/app/views/landing-page/landing-page.component.ts
+++ frontend/src/app/views/landing-page/landing-page.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { DashboardComponent } from "../dashboard/dashboard.component";
 import { ImageLettersComponent } from '../../components/landing-page/image-letters/image-letters.component';
 import { PreArticleComponent } from '../../components/landing-page/pre-article/pre-article.component';
+import { ArticlesComponent } from '../../components/landing-page/articles/articles.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavbarComponent, DashboardComponent, ImageLettersComponent, PreArticleComponent],
+  imports: [NavbarComponent, DashboardComponent, ImageLettersComponent, PreArticleComponent, ArticlesComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
