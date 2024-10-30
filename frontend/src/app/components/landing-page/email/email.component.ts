47df8086752bf33fb3138aab632f78b62536cf95
angelporlan <angeelporlan4@gmail.com>
2024-10-30 17:55:52 +0100

    email component of landing page finished

---
+++ frontend/src/app/components/landing-page/email/email.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-email',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './email.component.html',
+  styleUrl: './email.component.css'
+})
+export class EmailComponent {
+
+}
--- frontend/src/app/views/landing-page/landing-page.component.ts
+++ frontend/src/app/views/landing-page/landing-page.component.ts
@@ -4,11 +4,12 @@ import { DashboardComponent } from "../dashboard/dashboard.component";
 import { ImageLettersComponent } from '../../components/landing-page/image-letters/image-letters.component';
 import { PreArticleComponent } from '../../components/landing-page/pre-article/pre-article.component';
 import { ArticlesComponent } from '../../components/landing-page/articles/articles.component';
+import { EmailComponent } from '../../components/landing-page/email/email.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavbarComponent, DashboardComponent, ImageLettersComponent, PreArticleComponent, ArticlesComponent],
+  imports: [NavbarComponent, DashboardComponent, ImageLettersComponent, PreArticleComponent, ArticlesComponent, EmailComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
