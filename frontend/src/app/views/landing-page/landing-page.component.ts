2f13fb613e37b2982290dd3698ece2b6f0f20bf1
angelporlan <angeelporlan4@gmail.com>
2024-11-01 16:20:34 +0100

    footer

--- frontend/src/app/views/landing-page/landing-page.component.ts
+++ frontend/src/app/views/landing-page/landing-page.component.ts
@@ -5,11 +5,12 @@ import { ImageLettersComponent } from '../../components/landing-page/image-lette
 import { PreArticleComponent } from '../../components/landing-page/pre-article/pre-article.component';
 import { ArticlesComponent } from '../../components/landing-page/articles/articles.component';
 import { EmailComponent } from '../../components/landing-page/email/email.component';
+import { FooterComponent } from '../../components/landing-page/footer/footer.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavbarComponent, DashboardComponent, ImageLettersComponent, PreArticleComponent, ArticlesComponent, EmailComponent],
+  imports: [NavbarComponent, DashboardComponent, ImageLettersComponent, PreArticleComponent, ArticlesComponent, EmailComponent, FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
